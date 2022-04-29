1225cb31d7024c72ea6fa2299db7c44e264e0915
Philip Owusu Asare <philip.asare@amalitech.org>
2022-04-29 12:12:35 +0000

    creating configs

--- index.php
+++ index.php
@@ -2,3 +2,8 @@
 declare(strict_types = 1);
 
 require_once __DIR__ .'/vendor/autoload.php';
+
+// use Philipowusuasare\BugReport\Helpers\Config;
+
+$config = Philipowusuasare\BugReport\Helpers\Config::get('app');
+var_dump($config);
---
+++ src/Configs/app.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'app_name' => 'Bug Report App',
+    'env' => 'local',
+    'debug' => true,
+    'log' => __DIR__ . '/../Logger',
+];
